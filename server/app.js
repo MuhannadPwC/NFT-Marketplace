19b1830f49e53deedb98f0f1c458e02aa0ad51fb
Muhannad Al Omari <muhannad.al-omari@pwc.com>
2023-03-29 10:53:32 +0300

    initial commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is Listening on ${port}`);
+});
