3541199a32dec6755dae38faaf08ff689fe70280
Muhannad Al Omari <muhannad.al-omari@pwc.com>
2023-05-11 14:56:45 +0300

    add: terrible scroll animation to nft scroller

--- frontend/src/components/Home/NftScroller.tsx
+++ frontend/src/components/Home/NftScroller.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  ChakraComponent,
   Container,
   Flex,
   Grid,
@@ -7,11 +8,43 @@ import {
   Image,
   useColorMode,
 } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
 import image from "../../assets/image.png";
 
 const NftScroller = () => {
   const { colorMode } = useColorMode();
+  const firstColumn = useRef(null);
+  const secondColumn = useRef(null);
+  const lastColumn = useRef(null);
+  let index = true;
 
+  setInterval(() => {
+    firstColumn.current.scroll({
+      top: index ? 9000 : 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    index = !index;
+  }, 3000);
+
+  setInterval(() => {
+    secondColumn.current.scroll({
+      top: index ? 9000 : 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    index = !index;
+  }, 3000);
+
+  setInterval(() => {
+    lastColumn.current.scroll({
+      top: index ? 9000 : 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    index = !index;
+  }, 3000);
+  
   return (
     <Box
       h={"600px"}
@@ -23,7 +56,7 @@ const NftScroller = () => {
       }
     >
       <Grid h={"100%"} templateColumns="repeat(3, 1fr)" gap={4}>
-        <GridItem h={"100%"} overflow={"scroll"}>
+        <GridItem h={"100%"} overflow={"scroll"} ref={firstColumn}>
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
@@ -32,7 +65,7 @@ const NftScroller = () => {
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
         </GridItem>
-        <GridItem h={"100%"} overflow={"scroll"} pt={"10"}>
+        <GridItem h={"100%"} overflow={"scroll"} pt={"10"} ref={secondColumn}>
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
@@ -41,7 +74,7 @@ const NftScroller = () => {
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
         </GridItem>
-        <GridItem h={"100%"} overflow={"scroll"} pt={"5"}>
+        <GridItem h={"100%"} overflow={"scroll"} pt={"5"} ref={lastColumn}>
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
           <Image src={image} h={"250px"} mb={4} borderRadius="3xl" />
